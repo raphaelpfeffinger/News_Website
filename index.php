097d7b980ebe585752fa7243d5a47991a4d4324b
ruffy777 <raphael.pfeffinger@gmail.com>
2023-05-03 15:29:47 +0200

    make all files, for the moment insert logo

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Index</title>
+    </head>
+    <body>
+        <a href="index.php">Home</a>
+        <a href="index.php"><img src="logo.png"></a>
+        <div id="navigation">
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+        </div>
+        <?php 
+        ?>
+    </body>
+</html>
